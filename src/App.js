2fb64769f7d4664cdb04e1125495af496593cd10
Veronicahwambui <njengaveronicahwambui@gmail.com>
2022-05-08 22:43:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    let id = 1;
+    if(todos.length > 0) {
+      id = todos[0].id + 1
+    }
+    let todo = {id: id, text: text, completed: false, important: false}
+    let newTodos = [todo, ...todos]
+    setTodos(newTodos)
+  };
+
+  const removeTodo = (id) => {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodo} />
+      <hr className="seperator"/>
+
+      {todos.map((todo) => {
+        return (
+          <TodoItem removeTodo={removeTodo} completeTodo={completeTodo}todo={todo} key={todo.id}/>
+        )
+      })}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
